01ac70c481400ae427f204fa41d7420d47a2dd56
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-08-10 14:23:45 +0500

    animation fade animation

--- main.js
+++ main.js
@@ -7,5 +7,29 @@ col_btn.forEach((btn,index) => {
          col_bt.classList.remove("btn");
       })
       e.target.classList.add("btn");
+      let data_btn = btn.getAttribute("data-btn");
+      col_item.forEach((col,inde) => {
+         if (col.getAttribute("data-item") == data_btn || data_btn == "all") {
+            col.classList.remove("hide")
+         } else {
+            col.classList.add("hide");
+         }
+      })
    })
+})
+
+// Nav Bar
+let ul = document.querySelector("ul");
+let burger_icon = document.querySelector(".burger_icon");
+
+burger_icon.addEventListener("click", () => {
+   if (burger_icon.classList.contains("fa-bars")) {
+      burger_icon.classList.add("fa-xmark");
+      burger_icon.classList.remove("fa-bars");
+      ul.classList.add("active");
+   } else {
+      burger_icon.classList.remove("fa-xmark");
+      burger_icon.classList.add("fa-bars");
+      ul.classList.remove("active");
+   }
 })
